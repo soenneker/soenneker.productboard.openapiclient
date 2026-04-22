aa1bca601b92ca0ae2278a19c867a0c1dc2af69b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:31 -0500

    TUnit conversion

--- test/Soenneker.ProductBoard.OpenApiClient.Tests/ProductBoardOpenApiClientTests.cs
+++ test/Soenneker.ProductBoard.OpenApiClient.Tests/ProductBoardOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.ProductBoard.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class ProductBoardOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class ProductBoardOpenApiClientTests : HostedUnitTest
 {
-    public ProductBoardOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public ProductBoardOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
